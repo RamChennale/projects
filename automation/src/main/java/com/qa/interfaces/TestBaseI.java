3ffda46b15cd70ecf88011f227426435ed7977e7
pari <pari@DESKTOP-Q0AM94O>
2020-07-10 11:06:58 +0530

    added all TestBase class abstract methods

--- automation/src/main/java/com/qa/interfaces/TestBaseI.java
+++ automation/src/main/java/com/qa/interfaces/TestBaseI.java
@@ -1,9 +1,16 @@
 package com.qa.interfaces;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.testng.ITestResult;
 
 public interface TestBaseI {
 
-	public void initiate() throws IOException ;
-	public void tearDown();
+	public abstract void initiate() throws IOException ;
+	public abstract void reportSetup();
+	public abstract void getTestCaseName(Method method);
+	public abstract void getTestCaseResult(ITestResult result) throws IOException;
+	public abstract void flushReport();
+	public abstract void tearDown();
 }
