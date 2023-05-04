15ca3b6e6944472ef2175083dca359d942175f33
RamChennale <chennaleram@gmail.com>
2023-05-04 20:50:08 +0530

    new commit

--- selenium/src/com/qa/stdBrowser/Browser.java
+++ selenium/src/com/qa/stdBrowser/Browser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.google.api.client.util.GenericData.Flags;
@@ -11,43 +12,65 @@ import com.qa.stdReports.LoggerHelper;
 
 public class Browser {
 
-	BrowserInterface browser; 
-	String browserName; 
+	BrowserInterface browser;
+	String browserName;
 	Logger logger = LoggerHelper.getLogger(Browser.class);
-	
-	public Browser (String browserName , String driverName) {
+
+	public Browser(String browserName, String driverName) {
 		this.browserName = browserName;
 		close(driverName);
-		
+
 		switch (browserName.toLowerCase()) {
 		case "chrome":
-			System.getProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resource/drivers/" + driverName);
+			System.getProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "/src/test/resource/drivers/" + driverName);
 			browser = new ChromeBrowser();
 			break;
 
+		case "firefox":
+			browser = new FirefoxBrowser();
+			break;
+
+		case "ie":
+			System.getProperty("webdriver.ie.driver",
+					System.getProperty("user.dir") + "/src/test/resource/drivers/" + driverName);
+			browser = new IEBrowser();
+			break;
+
+		case "edge":
+			System.getProperty("webdriver.edge.driver",
+					System.getProperty("user.dir") + "/src/test/resource/drivers/" + driverName);
+			browser = new EdgeBrowser();
+			break;
+
 		default:
+			logger.debug("Browser" + browserName + "is not in this libarary");
 			break;
 		}
 	}
-	
+
+	public WebDriver getDriver() {
+		return browser.getDriver();
+	}
+
 	public void close(String processName) {
 		try {
 			if (isProcessRunning(processName)) {
-				
+
 			}
 		} catch (Exception e) {
 			logger.debug("Failed to kill the process" + processName);
 		}
 	}
-	
+
 	public static boolean isProcessRunning(String processName) {
-	
+
 		boolean running_value = false;
 		try {
-			String line; 
+			String line;
 			Process process = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32" + "tasklist.exe");
 			BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			while((line = input.readLine()) !=null) {
+			while ((line = input.readLine()) != null) {
 				if (line.contains(processName)) {
 					running_value = true;
 					break;
@@ -61,4 +84,3 @@ public class Browser {
 		return running_value;
 	}
 }
-
